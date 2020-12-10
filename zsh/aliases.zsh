482f440851452aa13231fdbec69118b1d013bb95
Justin Tomich <tomichj@gmail.com>
2020-12-09 21:12:55 -0800

    c -> p, aliases for: .., ..., ...., c, l

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,9 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+alias c='clear'
+alias l='ls -la'
+
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
