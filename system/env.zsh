d030e822f5a9c6c22499b2a405dca780b3f9a096
Justin Tomich <tomichj@gmail.com>
2022-06-05 17:01:25 -0700

    environment config, simple scripts for process control
    - editor defaults to emacs, falls back to mg
    - adds support for z
    - sets PAGER, LESS
    - murder - kills a process
    - running - lists running files

--- system/env.zsh
+++ system/env.zsh
@@ -1,2 +1,22 @@
-# export EDITOR='code'
-export EDITOR='emacs'
+if hash emacs 2>/dev/null; then
+  export EDITOR=emacs
+else
+  export EDITOR=mg
+fi
+export USE_EDITOR="$EDITOR"
+export VISUAL="$EDITOR"
+
+export LC_ALL='en_US.UTF-8'
+export LANG='en_US.UTF-8'
+export LANGUAGE='en_US.UTF-8'
+
+export PAGER=less
+
+export LESS='--ignore-case --QUIET --raw-control-chars --no-init --quit-if-one-screen'
+export LESSHISTFILE="$XDG_CACHE_HOME/lesshst"
+export LESSSECURE=1
+
+# Fire up z
+if [[ -f "/opt/homebrew/etc/profile.d/z.sh" ]]; then
+	. /opt/homebrew/etc/profile.d/z.sh
+fi
