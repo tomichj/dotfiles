8034be05ff4fd7050cb99ea8139107df28a5cdea
Justin Tomich <tomichj@gmail.com>
2025-08-18 21:41:00 -0700

    tweaks for homebrew, docker completions

---
+++ docker/completions.zsh
@@ -0,0 +1,5 @@
+# The following lines have been added by Docker Desktop to enable Docker CLI completions.
+fpath=(/Users/tomichj/.docker/completions $fpath)
+autoload -Uz compinit
+compinit
+# End of Docker CLI completions
--- homebrew/path.zsh
+++ homebrew/path.zsh
@@ -1,1 +1,11 @@
-export PATH="/opt/homebrew/bin:$PATH"
+#export BREW_CASK_BIN="/Volumes/w1/Applications"
+#export HOMEBREW_CASK_OPTS="--appdir=${BREW_CASK_BIN}"
+
+export BREW_CASK_BIN="/Applications"
+export HOMEBREW_CASK_OPTS=""
+
+export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:${BREW_CASK_BIN}:$PATH"
+if [ -d ${BREW_CASK_BIN}/Postgres.app/Contents/Versions/latest/bin ]; then
+    export PATH="${BREW_CASK_BIN}/Postgres.app/Contents/Versions/latest/bin:${PATH}"
+fi
+
